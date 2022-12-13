63c17a57fef482353daf7b78a98498676e3a3895
Aditya Kumar <adityashu6799@gmail.com>
2022-12-13 12:52:10 +0530

    modified notes program in lecture-8

--- lecture-8/notes/notes.cpp
+++ lecture-8/notes/notes.cpp
@@ -3,17 +3,35 @@ using namespace std;
 
 int main()
 {
-    int money = 1330;
+    int money = 1770;
     int n100 = 0, n50 = 0, n20 = 0, n1 = 0;
 
-    switch (money)
+    switch (1)
     {
-    case:
-        n100++;
-        money -= 100;
+    case 1:
+        n100 = money / 100;
+        money -= 100 * n100;
+
+    case 2:
+        n50 = money / 50;
+        money -= 50 * n50;
+
+    case 3:
+        n20 = money / 20;
+        money -= 20 * n20;
+
+    case 4:
+        n1 = money / 1;
+        money -= 1 * n1;
         break;
 
     default:
         break;
     }
+
+    cout << "number of notes :" << endl;
+    cout << "Rs. 100 notes : " << n100 << endl;
+    cout << "Rs. 50 notes : " << n50 << endl;
+    cout << "Rs. 20 notes : " << n20 << endl;
+    cout << "Rs. 1 notes : " << n1 << endl;
 }
